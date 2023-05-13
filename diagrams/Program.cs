b10f697395e8afa07f83f4b6fdaf5c11c7f7f6a9
Veskel01 <jakubandrz1@gmail.com>
2023-05-13 18:53:35 +0200

    fix: fix species info printing

--- diagrams/Program.cs
+++ diagrams/Program.cs
@@ -1,14 +1,25 @@
 ﻿using diagrams;
+using diagrams.Species.Ionia;
 using diagrams.Species.Noxus;
 
 var app = new App();
 
-app.createChampion(new NoxusChampionFactory());
+var noxusChampionFactory = new NoxusChampionFactory();
+var ioniaChampionFactory = new IoniaChampionFactory();
 
-var heros = app.GetHeros();
-var region = app.GetRegion();
-var role = app.GetRole();
+void PrintAppProperties()
+{
+    var heros = app.GetHeros();
+    var region = app.GetRegion();
+    var role = app.GetRole();
 
-Console.WriteLine($"Heros: {heros?.GetDescription()}");
-Console.WriteLine($"Region: {region?.GetDescription()}");
-Console.WriteLine($"Role: {role?.GetDescription()}");
+    Console.WriteLine($"Heros: {heros?.GetDescription()}");
+    Console.WriteLine($"Region: {region?.GetDescription()}");
+    Console.WriteLine($"Role: {role?.GetDescription()}");
+}
+
+app.createChampion(noxusChampionFactory);
+PrintAppProperties();
+
+app.createChampion(ioniaChampionFactory);
+PrintAppProperties();
